a6a4782efbe6730cb2e2e592030a8e6ed3ca1ae4
Nuno Santos <you.leggera@gmail.com>
2020-10-06 09:58:42 +0100

    third component changes

--- src/ComponentThree.js
+++ src/ComponentThree.js
@@ -4,7 +4,10 @@ import './AllStyles.css'
 function ComponentThree(){
     return(
         <div className='ComponentThree'>
-          <h1 className='h1'>This is only other component</h1>
+          <h1 className='h1'>Portefolio</h1>
+          <a className='a1'href="https://messenger-clone-0.web.app/" >My Messenger Clone App</a>
+          <br></br>
+          <a className='a1'href="https://instagram-clone-0.web.app/" >My Instagram Clone App</a>
         </div>
     );
 }
