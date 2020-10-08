0a922c96d97f59c3aca0d12c27c14ab8c2d6ae8f
Nuno Santos <you.leggera@gmail.com>
2020-10-08 18:35:05 +0100

    add amazon

--- src/ComponentThree.js
+++ src/ComponentThree.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import './AllStyles.css'
+import React from "react";
+import "./AllStyles.css";
 
-function ComponentThree(){
-    return(
-        <div className='ComponentThree'>
-          <h1 className='h1'>Portefolio</h1>
-          <a className='a1'href="https://messenger-clone-0.web.app/" >My Messenger Clone App</a>
-          <br></br>
-          <a className='a1'href="https://instagram-clone-0.web.app/" >My Instagram Clone App</a>
-        </div>
-    );
+function ComponentThree() {
+  return (
+    <div className="ComponentThree">
+      <h1 className="h1">Portefolio</h1>
+      <a className="a1" href="https://messenger-clone-0.web.app/">
+        My Messenger Clone App
+      </a>
+      <br></br>
+      <a className="a1" href="https://instagram-clone-0.web.app/">
+        My Instagram Clone App
+      </a>
+      <br></br>
+      <a className="a1" href="https://netflix-clone-3.web.app/">
+        My Netflix Clone App
+      </a>
+      <br></br>
+      <a className="a1" href="https://amzncloneapp2.web.app/">
+        My Amazon Clone App
+      </a>
+    </div>
+  );
 }
 
-export default ComponentThree
+export default ComponentThree;
